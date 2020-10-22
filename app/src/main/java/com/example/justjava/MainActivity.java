17896ffb736fbf689ec5b3fc3d01b713548488ce
ddsultan <ddsultan@hotmail.com>
2020-10-22 13:22:24 +0200

    Add simple coffee order form

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.displayQuantity();
+    }
+
+    public void onOrderButtonClick(View view) {
+        final TextView priceTextView = findViewById(R.id.price_text_view);
+
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(5 * this.quantity));
+    }
+
+    public void onIncrementButtonClick(View view) {
+        this.quantity++;
+        displayQuantity();
+    }
+
+    public void onDecrementButtonClick(View view) {
+        if (this.quantity > 0) this.quantity--;
+        displayQuantity();
+    }
+
+    private void displayQuantity() {
+        final TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + this.quantity);
     }
 }
